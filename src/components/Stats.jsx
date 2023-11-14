469c2dddb8a2f6885ad04112b567fe97d5846bda
Sneha Purkayastha <iamsneha0296@gmail.com>
2023-11-14 14:21:32 +0530

    completed stats section

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { stats } from '../constants';
+import styles from '../style';
 
 const Stats = () => {
   return (
-    <div>Stats</div>
+    <>
+      <section className={`${styles.flexCenter} flex-row flex-wrap sm:mb-20 mb-6`}>
+        {stats.map((stat) => (
+          <div key={stat.id} className={`flex-1 flex justify-start flex-row items-center m-3`}>
+            <h4 className='font-poppins font-semibold xs:text-[40px] text-white xs:leading-[53px] leading-[43px] text-[30px]'>{stat.value}</h4>
+            <p className='font-poppins font-normal xs:text-[20px] xs:leading-[26px] leading-[21px] text-[15px] text-gradient uppercase'>{stat.title}</p>
+          </div>
+        ))}
+      </section>
+    </>
   )
 }
 
